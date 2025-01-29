dbd826f471683c1782d9a32de465886efc8bd0ef
Tony Zan <tonyzan@Tonys-MacBook-Air.local>
2025-01-28 18:19:50 -0800

    NEW

---
+++ Assets/audioManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioManager : MonoBehaviour
+{
+    public AudioSource roar; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        roar.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
